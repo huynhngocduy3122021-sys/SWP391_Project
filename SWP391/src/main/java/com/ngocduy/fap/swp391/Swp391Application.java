7429e2adc19e8c679d27efb8be7813a74b9c03a3
huynhngocduy <huynhngocduy3122021@gmail.com>
2025-10-01 12:59:55 +0700

    Inital commit

---
+++ SWP391/src/main/java/com/ngocduy/fap/swp391/Swp391Application.java
@@ -0,0 +1,14 @@
+package com.ngocduy.fap.swp391;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swp391Application.class, args);
+        System.out.println("SWP391");
+    }
+
+}
---
+++ SWP391/src/test/java/com/ngocduy/fap/swp391/Swp391ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngocduy.fap.swp391;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp391ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
