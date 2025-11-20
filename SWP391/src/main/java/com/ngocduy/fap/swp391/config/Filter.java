f05870fd80ee0e407743b10a947b7696d91328b5
Dkain <haophong660@gmail.com>
2025-11-20 22:31:20 +0700

    public api article

--- SWP391/src/main/java/com/ngocduy/fap/swp391/config/Filter.java
+++ SWP391/src/main/java/com/ngocduy/fap/swp391/config/Filter.java
@@ -37,6 +37,7 @@ public class Filter extends OncePerRequestFilter {
             "POST:/api/members/login",
             "POST:/api/payment/**",
             "GET:/api/payment/**",
+            "GET:/api/article",
             "PATCH:/api/payment/**",
             "GET:/swagger-ui/**",
             "GET:/v3/api-docs/**",
